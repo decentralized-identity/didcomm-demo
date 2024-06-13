91b87c1aba267b16ed1fb7c59e591e6de184fd2d
Colton Wolkins (Laptop) <colton@indicio.tech>
2024-06-12 21:57:46 -0600

    feat: update Indicio mediator did
    Signed-off-by: Colton Wolkins (Laptop) <colton@indicio.tech>

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const DEFAULT_MEDIATOR =
-  "did:web:us-east.public.mediator.indiciotech.io"
+  "did:web:us-east2.public.mediator.indiciotech.io"
 
 export const ROOTS_MEDIATOR =
   "did:peer:2.Ez6LSms555YhFthn1WV8ciDBpZm86hK9tp83WojJUmxPGk1hZ.Vz6MkmdBjMyB4TS5UbbQw54szm8yvMMf1ftGV2sQVYAxaeWhE.SeyJpZCI6Im5ldy1pZCIsInQiOiJkbSIsInMiOiJodHRwczovL21lZGlhdG9yLnJvb3RzaWQuY2xvdWQiLCJhIjpbImRpZGNvbW0vdjIiXX0"
